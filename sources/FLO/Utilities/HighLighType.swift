2e9feb6993e95ce00fa7e4a4a3ddef37341ec98a
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2024-05-02 20:15:47 +0900

    Refactoring: viewModel에 종속되어 있던 enum 별도 파일로 관리

---
+++ sources/FLO/Utilities/HighLighType.swift
@@ -0,0 +1,13 @@
+//
+//  HighLighType.swift
+//  FLO
+//
+//  Created by 김성준 on 5/2/24.
+//
+
+import Foundation
+
+enum HighlighType {
+    case HighLight
+    case nonHighLight
+}
---
+++ sources/FLO/Utilities/TimeType.swift
@@ -0,0 +1,13 @@
+//
+//  TimeType.swift
+//  FLO
+//
+//  Created by 김성준 on 5/2/24.
+//
+
+import Foundation
+
+enum TimeType {
+    case milSeconds
+    case seconds
+}
