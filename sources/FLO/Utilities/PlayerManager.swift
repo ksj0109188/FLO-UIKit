060c0b2ea43881f57ce8c7404549ec111fab80ea
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2024-05-07 23:29:54 +0900

    Feature: AVPlayer 추가 메소드 개발

--- sources/FLO/Utilities/PlayerManager.swift
+++ sources/FLO/Utilities/PlayerManager.swift
@@ -6,43 +6,66 @@
 //
 
 import AVKit
+import Combine
 
 protocol PlayableViewModel {
     var playerManager: PlayerManager {get}
 }
+protocol playerMangerInput {
+    func configure(url: URL)
+    func togglePlayPause()
+    func moveToTimeLine(to time: CMTime)
+}
 
-final class PlayerManager {
+protocol playerMangerOutput {
+    var isPausedSubject: CurrentValueSubject<Bool, Never> { get }
+    func observer(update: @escaping (CMTime) -> Void)
+    func playerTime() -> CMTime
+}
+
+typealias playerMangerInOutput = playerMangerInput & playerMangerOutput
+
+final class PlayerManager: playerMangerInOutput {
     private var player: AVPlayer!
     private var timeObserverToken: Any?
+    private var isPlayerPuased: Bool = true
+    let isPausedSubject = CurrentValueSubject<Bool, Never>(true)
     
     deinit {
         removeObserver()
     }
     
-    func configure(url: URL) {
-        player = AVPlayer(url: url)
-    }
-    
-    func removeObserver() {
+    private func removeObserver() {
         if let token = timeObserverToken {
             player.removeTimeObserver(token)
             timeObserverToken = nil
         }
     }
     
+    func configure(url: URL) {
+        player = AVPlayer(url: url)
+    }
+    
     func observer(update: @escaping (CMTime) -> Void) {
         let interval = CMTime(seconds: 0.5,
                               preferredTimescale: CMTimeScale(NSEC_PER_SEC))
         
-        timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main,using: update)
+        timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main, using: update)
+        
     }
     
-    func pausePlayer() {
-        player.rate == 0 ? player.play() : player.pause()
+    func togglePlayPause() {
+        isPlayerPuased ? player.play() : player.pause()
+        isPlayerPuased.toggle()
+        
+        isPausedSubject.send(isPlayerPuased)
     }
     
     func moveToTimeLine(to time: CMTime) {
         player.seek(to: time)
     }
     
+    func playerTime() -> CMTime {
+        return player.currentTime()
+    }
 }
